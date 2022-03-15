935dfc6c42dfd07ea29ee5c544c9c14990ef596d
Stan <stantonmoore42@gmail.com>
2022-03-14 20:04:10 -0400

    Edit Working

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -5,15 +5,20 @@ const DefaultLayout = require('./layout/Default.jsx')
 class Edit extends React.Component{
   render() {
     return (
+      <div>
       <DefaultLayout title="Edit Page"> 
       <link rel="stylesheet" href="/css/app.css"/>         
-     {/* See the Layout takes in a prop called Title and we pass Edit Page to it  note: comments can't go first or last in  jsx return*/}
-          {/* form is not complet we will do that below*/}
+      {/* See the Layout takes in a prop called Title and we pass Edit Page to it  note: comments can't go first or last in  jsx return*/}
         <form action={`/products/${this.props.product._id}?_method=PUT`} method="POST">
-          <input type="submit" value="Submit Changes"/>
-      </form>
+          Name: <input type="text" name="name" defaultValue={this.props.product.name}/><br/>
+          Image: <input type="text" name="img" defaultValue={this.props.product.img}/><br/>
+          Description: <input type="text" name="description" defaultValue={this.props.product.description}/><br/>
+          Price: <input type="text" name="price" defaultValue={this.props.product.price}/><br/>
+          Quantity: <input type="text" name="qty" defaultValue={this.props.product.qty}/><br/>              
+          <input type="submit" name="" value="Edit"/>
+        </form>
       </DefaultLayout>
-    )
-  }
+      </div>);
+}
 }
 module.exports= Edit;
